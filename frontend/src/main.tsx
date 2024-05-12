4409634eaf5bd97f2c424067067cb6333ed0c60d
Anton Melnikov <melnikov.anton@gmail.com>
2024-05-12 13:11:18 +0300

    TailwindCSS installed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,14 @@
+// Lib
+// -----------------------------------------------------------------------------
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// App
+// -----------------------------------------------------------------------------
+import "./styles/main.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hello world!</h1>
+    <h1 className="text-4xl font-bold text-orange-600">Hello world!</h1>
   </React.StrictMode>,
 );
