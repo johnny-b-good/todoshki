6b87eb929c2b83999903ee31fe8ea9e47f844e4b
Anton Melnikov <melnikov.anton@gmail.com>
2024-05-12 12:16:30 +0300

    Pretty print logs (including Prisma logs)

--- backend/src/plugins/prismaPlugin.ts
+++ backend/src/plugins/prismaPlugin.ts
@@ -8,7 +8,30 @@ declare module "fastify" {
 }
 
 export const prismaPlugin = fastifyPlugin(async (fastify) => {
-  const prismaClient = new PrismaClient();
+  const prismaClient = new PrismaClient({
+    log: [
+      { emit: "event", level: "query" },
+      { emit: "event", level: "warn" },
+      { emit: "event", level: "info" },
+      { emit: "event", level: "error" },
+    ],
+  });
+
+  prismaClient.$on("error", (ev) => {
+    fastify.log.error(ev, ev.message);
+  });
+
+  prismaClient.$on("info", (ev) => {
+    fastify.log.info(ev, ev.message);
+  });
+
+  prismaClient.$on("query", (ev) => {
+    fastify.log.info(ev, "database query");
+  });
+
+  prismaClient.$on("warn", (ev) => {
+    fastify.log.warn(ev, ev.message);
+  });
 
   await prismaClient.$connect();
 
